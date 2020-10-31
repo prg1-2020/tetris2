b66b560560adc1b4f1e43648f10a5746c58ac842
sakurai akira <akilucky0304@gmail.com>
2020-10-31 13:10:44 +0900

    微調整

--- src/tetris/scala/tetris.scala
+++ src/tetris/scala/tetris.scala
@@ -115,7 +115,7 @@ case class TetrisWorld(piece: ((Int, Int), S.Shape), pile: S.Shape, hold: S.Shap
   }
 */
   def tick(): World = {
-    if(piece._2 == List(List(Transparent)) || stop || eCount >= 100) return this
+    if(piece._2 == List(List(Transparent)) || stop || eCount >= 1) return this
     val cur_x = piece._1._1
     val cur_y = piece._1._2
     if(collision(TetrisWorld(((cur_x, cur_y + 1), piece._2) , pile, hold, stop, next, nextNext, cCount, score, eCount))) {
@@ -128,7 +128,7 @@ case class TetrisWorld(piece: ((Int, Int), S.Shape), pile: S.Shape, hold: S.Shap
         }
         else TetrisWorld(((WellWidth / 2 - 1, 0), next), nextPile, hold, stop, nextNext, S.random(), 0, score + add, eCount)
       }
-      if(eCount + eraseCount >= 100){
+      if(eCount + eraseCount >= 1){
         println("clear!") //強制クリア
         println(s"score: ${score + add}")
       }
@@ -179,7 +179,7 @@ case class TetrisWorld(piece: ((Int, Int), S.Shape), pile: S.Shape, hold: S.Shap
       }
       case _ => TetrisWorld(piece, pile, hold, stop, next, nextNext, cCount, score, eCount)
     }
-    if(collision(nextWorld) || eCount >= 100) {
+    if(collision(nextWorld) || (eCount >= 1 && nextWorld.score != 0)) {
       return this
     }
     else if(newStop){
