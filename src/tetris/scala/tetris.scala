fd4e3c9356183c87c6432a7c15106d2548389982
tomohiro-yamada <y.tomo.0524@gmail.com>
2020-11-09 19:15:15 +0900

    ハードドロップ、ゴーストブロック、テトリミノ変更(練習用)

--- src/tetris/scala/tetris.scala
+++ src/tetris/scala/tetris.scala
@@ -17,6 +17,7 @@ import sgeometry.Pos
 import sdraw.{World, Color, Transparent, HSB}
 
 import tetris.{ShapeLib => S}
+import java.awt.Shape
 
 // テトリスを動かすための関数
 case class TetrisWorld(piece: ((Int, Int), S.Shape), pile: S.Shape) extends World() {
@@ -48,6 +49,13 @@ case class TetrisWorld(piece: ((Int, Int), S.Shape), pile: S.Shape) extends Worl
 
   // shape の描画（原点）
   def drawShape00(shape: S.Shape): Boolean = drawShape((0, 0), shape)
+  
+  //目的:テトリミノをそのままハードドロップしたときのイメージを表示する。
+  def drawGhostBlock(pos: (Int, Int), shape: S.Shape): Boolean = {
+    val ghostShape = shape.map(_.map((b: S.Block) => if(b==Transparent) b else sdraw.DarkGray))
+    val ((x:Int,y:Int),s:S.Shape) = hardDrop(pos,ghostShape)
+    drawShape((x,y),s)
+  }
 
   // ゲーム画面の描画
   val CanvasColor = HSB(0, 0, 0.1f)
@@ -57,30 +65,88 @@ case class TetrisWorld(piece: ((Int, Int), S.Shape), pile: S.Shape) extends Worl
     canvas.drawRect(Pos(0, 0), canvas.width, canvas.height, CanvasColor) &&
     drawShape00(pile) &&
     drawShape(pos, shape)
+    drawGhostBlock(pos, shape)
   }
 
   // 1, 4, 7. tick
-  // 目的：
+  // 目的：時間の経過に応じて世界を更新する。
+  /*  1.
+    def tick(): World = {
+    val ((x,y), shape) = piece
+    TetrisWorld(((x, y+1),shape),  pile)
+  } */
+  /* 4.
   def tick(): World = {
-    TetrisWorld(piece, pile)
+    val ((x,y), shape) = piece
+    if(collision(TetrisWorld(((x, y+1),shape),  pile))) TetrisWorld(piece,  pile)
+    else TetrisWorld(((x, y+1),shape),  pile)
   }
-
+  */
+  def tick(): World = {
+    val ((x,y), shape) = piece
+    val NewWorld = TetrisWorld(A.newPiece(), eraseRows(S.combine(S.shiftSE(shape,x,y), pile)))
+    if(collision(TetrisWorld(((x, y+1),shape), pile))) if(collision(NewWorld)) TetrisWorld(A.newPiece(), List.fill(A.WellHeight)(List.fill(A.WellWidth)(Transparent))) else NewWorld
+    else TetrisWorld(((x, y+1),shape), pile)
+  }
   // 2, 5. keyEvent
-  // 目的：
+  // 目的：キー入力に従って世界を更新する。
+  /* 2.
   def keyEvent(key: String): World = {
-    TetrisWorld(piece, pile)
+    val ((x,y), shape) = piece
+    key match {
+      case "RIGHT" => TetrisWorld(((x+1, y),shape),  pile)
+      case "LEFT" => TetrisWorld(((x-1, y),shape),  pile)
+      case "UP" => TetrisWorld(((x, y),S.rotate(shape)),  pile) 
+    }
+  }
+  */
+  /* ハードドロップと特定のツモ練習用のテトリミノ変更ボタン*/
+  def keyEvent(key: String): World = {
+    val ((x,y), shape) = piece
+    val ((nextX, nextY), nextS) = key match {
+      case "RIGHT" => ((x+1, y), shape)
+      case "LEFT" => ((x-1, y), shape)
+      case "UP" => ((x, y), S.rotate(shape))
+      case "DOWN" => hardDrop((x,y),shape)
+      case "i" => ((A.WellWidth / 2 - 1, 0), S.shapeI)
+      case "j" => ((A.WellWidth / 2 - 1, 0), S.shapeJ)
+      case "t" => ((A.WellWidth / 2 - 1, 0), S.shapeT)
+      case "o" => ((A.WellWidth / 2 - 1, 0), S.shapeO)
+      case "z" => ((A.WellWidth / 2 - 1, 0), S.shapeZ)
+      case "l" => ((A.WellWidth / 2 - 1, 0), S.shapeL)
+      case "s" => ((A.WellWidth / 2 - 1, 0), S.shapeS)
+    }
+    val NextWorld = TetrisWorld(((nextX, nextY),nextS), pile)
+    if(collision(NextWorld)) TetrisWorld(piece, pile) else NextWorld
   }
 
   // 3. collision
-  // 目的：
+  // 目的：受け取った世界で衝突が起きているかを判定する。
   def collision(world: TetrisWorld): Boolean = {
-    false
+    val ((x,y), shape) = world.piece
+      if(x < 0 || x + S.maxRowLength(shape) > A.WellWidth) true
+      else if(y + shape.length > A.WellHeight) true
+      else if(S.overlap(S.shiftSE(shape,x,y),world.pile)) true
+      else false
   }
 
   // 6. eraseRows
-  // 目的：
+  // 目的：pile を受け取ったら、揃った行を削除する。
   def eraseRows(pile: S.Shape): S.Shape = {
-    pile
+    def eraseOnly(pile: S.Shape): S.Shape = {
+      pile match {
+        case Nil => Nil
+        case r::rs => if(S.blockCountPerRow(r)==A.WellWidth) eraseOnly(rs) else r::eraseOnly(rs)
+      }
+    }
+  S.empty(A.WellHeight-eraseOnly(pile).length,A.WellWidth) ++ eraseOnly(pile) 
+  }
+
+  //目的:テトリミノを可能な限り下に落とした時のpieceを求める。
+  def hardDrop(pos: (Int, Int), shape: S.Shape): ((Int,Int),S.Shape) = {
+    val ((x,y),s) = (pos,shape)
+    if(collision(TetrisWorld(((x, y+1),s), pile))) ((x, y),s)
+    else hardDrop((x, y+1),s)
   }
 }
 
